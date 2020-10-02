72dba3a1bea8854dcfcbcc4d3e3234265c1325b0
bkanister <b.kanister@gmail.com>
2020-10-02 14:04:53 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let typesOfBrackets = [],
+        index = 0,
+        counter = 0;
+    let arr = str.split('');
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        typesOfBrackets.push(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`);
+    }
+    console.log(typesOfBrackets)
+    while (counter <= 35) {
+        counter++;
+        typesOfBrackets.forEach(n => {
+            index = str.indexOf(n);
+            if (index !== -1) {
+                arr.splice(index, 2);
+                str = arr.join('');
+            }
+        });
+    }
+    return arr.length === 0;
+};
